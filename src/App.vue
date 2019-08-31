68f344b485331bce2218142391532ff0e7eb3dd7
Tamosch94 <tamer94@hotmail.de>
2019-08-31 20:31:14 +0200

    initial Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+		<app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+    export default {
+    	data: function() {
+			return {
+				quotes: [
+					'Just one quote to see something'
+				],
+				maxQuotes: 10
+			}
+		},
+		components: {
+			'app-quote-grid': QuoteGrid
+		}
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,21 @@
+// Holds all Quotes
+<template lang="html">
+	<div class="row">
+		<app-quote v-for="quote in quotes" >
+			{{ quote }}
+		</app-quote>
+	</div>
+</template>
+
+<script>
+	import Quote from './Quote.vue';
+	export default {
+			props: ['quotes'],
+			components: {
+				'app-quote': Quote
+			}
+	}
+</script>
+
+<style lang="css" scoped>
+</style>
