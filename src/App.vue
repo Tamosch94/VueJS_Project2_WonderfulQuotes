2a96c86870c11c4476c5db18d76461dde3b3275f
Tamosch94 <tamer94@hotmail.de>
2019-08-31 22:43:05 +0200

    Header with progress bar integrated
    Application finished

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+		<app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
 		<app-new-quote @quoteAdded="newQuote"></app-new-quote>
 		<app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
 		<div class="row">
@@ -15,6 +16,7 @@
 <script>
 	import QuoteGrid from './components/QuoteGrid.vue';
 	import NewQuote from './components/NewQuote.vue';
+	import Header from './components/Header.vue';
 
     export default {
     	data: function() {
@@ -27,13 +29,17 @@
 		},
 		methods: {
 			newQuote(quoteData) {
+				if (this.quotes.length >= this.maxQuotes) {
+					return alert("Ups! You reached the maximum amount of quotes, delete some quotes first!!");
+				}
 				this.quotes.push(quoteData);
 			},
 			deleteQuote(quoteIndex) {
-				this.quotes.splice(quoteIndex);
+				this.quotes.splice(quoteIndex, 1);
 			}
 		},
 		components: {
+			'app-header': Header,
 			'app-quote-grid': QuoteGrid,
 			'app-new-quote': NewQuote
 		},
